28aa093614600494b578718c5c261da07bcf4783
SSSergiy <sergey1991serg@gmail.com>
2023-03-21 07:22:59 +0200

    oien

--- expo-test-app/SVG/battery.tsx
+++ expo-test-app/SVG/battery.tsx
@@ -1,14 +1,14 @@
-import * as React from "react"
-import Svg, { Rect, Path } from "react-native-svg"
+import * as React from 'react';
+import Svg, { Path, Rect } from 'react-native-svg';
 
 function Battery(props) {
   return (
     <Svg
       width={25}
       height={12}
-      viewBox="0 0 25 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      viewBox='0 0 25 12'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
       {...props}
     >
       <Rect
@@ -18,19 +18,19 @@ function Battery(props) {
         width={21}
         height={10.3333}
         rx={2.16667}
-        stroke="#000"
+        stroke='#000'
       />
-      <Path opacity={0.4} d="M23.672 4v4a2.17 2.17 0 000-4z" fill="#000" />
+      <Path opacity={0.4} d='M23.672 4v4a2.17 2.17 0 000-4z' fill='#000' />
       <Rect
         x={2.67197}
         y={2.33331}
         width={18}
         height={7.33333}
         rx={1.33333}
-        fill="#000"
+        fill='#000'
       />
     </Svg>
-  )
+  );
 }
 
-export default Battery
+export default Battery;
--- expo-test-app/SVG/baxeCard.tsx
+++ expo-test-app/SVG/baxeCard.tsx
@@ -1,29 +1,29 @@
-import * as React from "react"
-import Svg, { G, Path, Defs, ClipPath } from "react-native-svg"
+import * as React from 'react';
+import Svg, { ClipPath, Defs, G, Path } from 'react-native-svg';
 
 function BaxeCard(props) {
   return (
     <Svg
       width={24}
       height={24}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
       {...props}
     >
-      <G clipPath="url(#clip0_0_375)">
+      <G clipPath='url(#clip0_0_375)'>
         <Path
-          d="M3.9 4h16.2c.239 0 .468.094.636.26A.883.883 0 0121 4.89V19.11a.883.883 0 01-.264.629.906.906 0 01-.636.26H3.9a.906.906 0 01-.636-.26A.883.883 0 013 19.11V4.89c0-.236.095-.462.264-.629A.906.906 0 013.9 4zm15.3 8H4.8v6.222h14.4V12zm0-3.556V5.778H4.8v2.666h14.4z"
-          fill="#fff"
+          d='M3.9 4h16.2c.239 0 .468.094.636.26A.883.883 0 0121 4.89V19.11a.883.883 0 01-.264.629.906.906 0 01-.636.26H3.9a.906.906 0 01-.636-.26A.883.883 0 013 19.11V4.89c0-.236.095-.462.264-.629A.906.906 0 013.9 4zm15.3 8H4.8v6.222h14.4V12zm0-3.556V5.778H4.8v2.666h14.4z'
+          fill='#fff'
         />
       </G>
       <Defs>
-        <ClipPath id="clip0_0_375">
-          <Path fill="#fff" d="M0 0H24V24H0z" />
+        <ClipPath id='clip0_0_375'>
+          <Path fill='#fff' d='M0 0H24V24H0z' />
         </ClipPath>
       </Defs>
     </Svg>
-  )
+  );
 }
 
-export default BaxeCard
+export default BaxeCard;
